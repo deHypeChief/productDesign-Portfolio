029b67313c6cb0458f840e4f8eeea5de80915b22
deHypeChief <dev.hype7@gmail.com>
2026-02-14 09:15:58 +0100

    Wrap "Get in touch" button with anchor tag for improved navigation

--- src/assets/blocks/navbar.jsx
+++ src/assets/blocks/navbar.jsx
@@ -13,11 +13,12 @@ export function Navbar() {
                     <a href="#resume" className="font-medium">Resume</a>
                     <a href="#socials" className="font-medium">Socials</a>
                 </div>
-                <Button className="bg-[#FDBA0D] text-[#0B0B0B] hover:bg-[#FEEAB4] transition-colors duration-300 shadow-lg shadow-[#FEEAB4] hover:shadow-[#FDBA0D]">
+                <a href="mailto:Design@darkerhills.online"  className="md:hidden">
+                    <Button className="bg-[#FDBA0D] text-[#0B0B0B] hover:bg-[#FEEAB4] transition-colors duration-300 shadow-lg shadow-[#FEEAB4] hover:shadow-[#FDBA0D]">
                         <img src="/images/Vector.svg" alt="Envelope" />
-                    <p>Get in touch</p>
-                </Button>
-
+                        <p>Get in touch</p>
+                    </Button>
+                </a>
             </div>
         </nav>
     )
